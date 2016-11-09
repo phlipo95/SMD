eb34b6d13ebab33da72c6fe7380f970a32bae76b
Philip Schäfers <phil.schaefers@gmail.com>
2016-11-09 20:58:51 +0100

    test

--- 02-09.11.16/aufg4.py
+++ 02-09.11.16/aufg4.py
@@ -1,37 +1,64 @@
 def aufg4():
-    import matplotlib.pyplot as plt
     import numpy as np
-    from numpy.linalg import inv, det
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    from numpy.linalg import det
 
-    #Startwerte
-    myx = 4; myy = 2; sigmax = 3.5; sigmay = 1.5 
-    
-    #2dim Gaußfunktion
-    def phi(x1,x2):
-        return 1/(2*np.pi*sigmax*sigmay)*np.e**(-1/(2*sigmax**2)*(x1-myx)**2)*np.e**(-1/(2*sigmay**2)*(x2-myy)**2)
+    #Aufgabe4a
+    sigma_x = 3.5
+    sigma_y = 1.5
+    Cov = 4.2
+    rho = Cov/(sigma_x*sigma_y) #Korrelationskoeffizient
 
-    #Maximalwert der Gaußfunktion 
-    ephi = phi(4,2)/np.sqrt(np.e)
-    print('1/sqrt(e) der maximalen Stelle der Gaußfunktion:', ephi)
-    print(phi(1,1))
-    X = np.linspace(myx-sigmax-4, myx+sigmax+4, 100)
-    Y = np.linspace(myy-sigmay-4, myy+sigmay+4, 100)
-    x, y = np.meshgrid(X, Y) 
-    plt.figure() 
-    plt.pcolormesh(x, y, phi(x,y))
+    #Aufgabe4b
+    #Erwartungswerte
+    mu_x = 4
+    mu_y = 2
+    #Standardabweichung
+    sigma_x = 3.5
+    sigma_y = 1.5
+    #x- und y-Werte
+    n = 10**2 #Anzahl der Messwerte
+    x = np.linspace(mu_x-7, mu_x+7, n)
+    y = np.linspace(mu_y-7, mu_y+7, n)
+    #Kovarianz
+    Cov = 4.2
+    rho = Cov/(sigma_x*sigma_y)
+    #Kovarianzmatrix
+    CovM = 1/(1-rho**2) * np.matrix([[1,-rho],[-rho,1]])
+    #Determinante der Kovarianzmatrix
+    detCovM = det(CovM)
+
+    #2D-Gaußfunktion
+    def gauss(x, y):
+    	return 1/(2*np.pi*sigma_x*sigma_y) * np.e**(-0.5*(((x-mu_x)/sigma_x)**2 + ((y-mu_y)/sigma_y)**2))
+
+    x, y = np.meshgrid(x, y)
+
+    plt.figure()
+    ax = plt.gca(projection='3d')
+    ax.plot_surface(x, y, gauss(x, y), cmap = plt.get_cmap("jet"))
+    plt.savefig('build/3dGauß.pdf')
+    plt.close()
+
+    #Maximalwert der Gaußfunktion durch Wurzel(e)
+    maxGdurchE = gauss(mu_x, mu_y) / np.sqrt(np.e)
+    #Aufgabe 4b und 4c
+    plt.figure()
+    plt.pcolormesh(x, y, gauss(x,y))
     plt.colorbar()
-    plt.contour(x,y,phi(x,y), np.linspace(ephi,ephi,1), colors="k")
-    plt.plot(myx,myy,'bo', label='$\mu_{xy}$')
-    
-    #Standardabweichung eingezeichnete Punkte
-    plt.plot(myx+sigmax,myy, 'ro', label='$\sigma_{x/y}$')
-    plt.plot(myx-sigmax,myy, 'ro')
-    plt.plot(myx,myy+sigmay, 'ro')
-    plt.plot(myx,myy-sigmay, 'ro')
-    plt.axvline(x=myx, color='g', label='$\mu_x$')
-    plt.axhline(y=myy, label='$\mu_y$')
+    plt.contour(x, y, gauss(x,y), np.linspace(maxGdurchE, maxGdurchE, 1), colors="k")
+    plt.axvline(x=mu_x, color='r', label='$\mu_x$')
+    plt.plot(mu_x+sigma_x, mu_y, 'ro', label='$\mu_x \pm \sigma_x$')
+    plt.plot(mu_x-sigma_x, mu_y, 'ro')
+    plt.axhline(y=mu_y, color='g', label='$\mu_y$')
+    plt.plot(mu_x, mu_y+sigma_y, 'go', label='$\mu_y \pm \sigma_y$')
+    plt.plot(mu_x, mu_y-sigma_y, 'go')
     plt.legend(loc='best')
     plt.gca().set_aspect("equal")
-    plt.show()
+    plt.xlabel('x', fontsize='14')
+    plt.ylabel('y', fontsize='14')
+    plt.savefig('./build/2dGauß.pdf')
+    plt.close()
 if __name__ == '__main__':
     aufg4()
