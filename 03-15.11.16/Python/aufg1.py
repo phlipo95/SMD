bb62be9f0e8acbfc00043a3f8214b5313b7b8916
Philip Schäfers <phil.schaefers@gmail.com>
2016-11-10 14:23:30 +0100

    nr.1 und 2

---
+++ 03-15.11.16/Python/aufg1.py
@@ -0,0 +1,43 @@
+def aufg():
+	#Aufgabe 1c
+	def Z(E, M):
+		return (1+M/2**23) * 2**(E-127)
+	z = Z(0, 1)
+	print(z)
+
+	a = 1/(2*z)
+	print('a =', a)
+	print(a*z)
+
+	b = 2/(3*z)
+	print('b =', b)
+	print(b*z)
+
+	print('a')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	aufg()
---
+++ 03-15.11.16/Python/aufg2.py
@@ -0,0 +1,83 @@
+import ROOT
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+#Aufgabe 2a
+a = 1601; b = 3456; m = 10000
+def randFunk(x): #Zufallsgenerator
+    return (a * x + b)%m
+
+xn = np.array([1])
+for i in range(10000): #berechnet alle Zufallszahlen
+    xn = np.append(xn, randFunk(xn[i]))
+xn = xn/10000
+
+#Aufagbe 2b
+plt.hist(xn, bins=10)
+plt.xlabel('Zufallszahl')
+plt.ylabel('Anzahl')
+plt.savefig('Aufgabe2b.pdf')
+plt.close()
+
+#Aufgabe 2c
+xn_x = xn[0:]
+xn_y = np.append(xn[1:], xn[0])
+xn_z = np.append(xn[2:], (xn[0], xn[1]))
+
+plt.hist2d(xn_x, xn_y, bins=100)
+plt.xlabel('$x_i$')
+plt.ylabel('$x_{i+1}$')
+plt.colorbar()
+plt.savefig('Aufgabe2c1.pdf')
+plt.close()
+
+plt.figure()
+ax = plt.gca(projection='3d')
+for i in range(10000):
+    ax.scatter(xn_x[i], xn_y[i], xn_z[i], color='red')
+plt.savefig('Aufgabe2c2.pdf')
+plt.close()
+
+#Aufgabe 2d
+for i in range(10000): #Periodizität
+    if xn[i] == xn[0]:
+        print(i)
+
+#Aufgabe 2e
+xn = ROOT.TRandom3()
+xn = [xn.Rndm() for i in range(10000)]
+
+xn_x = xn[0:]
+xn_y = np.append(xn[1:], xn[0])
+xn_z = np.append(xn[2:], (xn[0], xn[1]))
+
+plt.hist(xn, bins=10)
+plt.xlabel('Zufallszahl')
+plt.ylabel('Anzahl')
+plt.savefig('Aufgabe2e1.pdf')
+plt.close()
+
+plt.hist2d(xn_x, xn_y, bins=1000)
+plt.xlabel('$x_i$')
+plt.ylabel('$x_{i+1}$')
+plt.colorbar()
+plt.savefig('Aufgabe2e2.pdf')
+plt.close()
+
+plt.figure()
+ax = plt.gca(projection='3d')
+for i in range(10000):
+    ax.scatter(xn_x[i], xn_y[i], xn_z[i], color='red')
+plt.savefig('Aufgabe2e3.pdf')
+plt.close()
+
+
+
+
+
+
+
+
+
+#
