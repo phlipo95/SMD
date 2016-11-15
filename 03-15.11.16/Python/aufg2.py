9ff4c12dae832dc82bf72d80615abd64d762efbe
Philip Schäfers <phil.schaefers@gmail.com>
2016-11-15 19:17:51 +0100

    test

--- 03-15.11.16/Python/aufg2.py
+++ 03-15.11.16/Python/aufg2.py
@@ -1,74 +1,78 @@
-import ROOT
-import numpy as np
-from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+def aufg():
 
-#Aufgabe 2a
-a = 1601; b = 3456; m = 10000
-def randFunk(x): #Zufallsgenerator
-    return (a * x + b)%m
+    import ROOT
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
 
-xn = np.array([1])
-for i in range(10000): #berechnet alle Zufallszahlen
-    xn = np.append(xn, randFunk(xn[i]))
-xn = xn/10000
-print(xn[1])
+    #Aufgabe 2a
+    a = 1601; b = 3456; m = 10000
+    def randFunk(x): #Zufallsgenerator
+        return (a * x + b)%m
 
-#Aufagbe 2b
-plt.hist(xn, bins=10)
-plt.xlabel('Zufallszahl')
-plt.ylabel('Anzahl')
-plt.savefig('Python/Aufgabe2b.pdf')
-plt.close()
+    xn = np.array([1])
+    for i in range(10000): #berechnet alle Zufallszahlen
+        xn = np.append(xn, randFunk(xn[i]))
+        xn = xn/10000
 
-#Aufgabe 2c
-xn_x = xn[0:]
-xn_y = np.append(xn[1:], xn[0])
-xn_z = np.append(xn[2:], (xn[0], xn[1]))
+    #Aufagbe 2b
+    plt.hist(xn, bins=10)
+    plt.xlabel('Zufallszahl')
+    plt.ylabel('Anzahl')
+    plt.savefig('Python/Aufgabe2b.pdf')
+    plt.close()
 
-plt.hist2d(xn_x, xn_y, bins=100)
-plt.xlabel('$(x_i, x_{i+1})$')
-plt.ylabel('$(x_{i+1}, x_{i+2})$')
-plt.colorbar()
-plt.savefig('Python/Aufgabe2c1.pdf')
-plt.close()
+    #Aufgabe 2c
+    xn_x = xn[0:]
+    xn_y = np.append(xn[1:], xn[0])
+    xn_z = np.append(xn[2:], (xn[0], xn[1]))
 
-plt.figure()
-ax = plt.gca(projection='3d')
-for i in range(10000):
-    ax.scatter(xn_x[i], xn_y[i], xn_z[i], color='red')
-plt.savefig('Python/Aufgabe2c2.pdf')
-plt.close()
+    plt.hist2d(xn_x, xn_y, bins=100)
+    plt.xlabel('$(x_i, x_{i+1})$')
+    plt.ylabel('$(x_{i+1}, x_{i+2})$')
+    plt.colorbar()
+    plt.savefig('Python/Aufgabe2c1.pdf')
+    plt.close()
 
-#Aufgabe 2d
-for i in range(10000): #Periodizität
-    if xn[i] == xn[0]:
-        print(i)
+    plt.figure()
+    ax = plt.gca(projection='3d')
+    for i in range(10000):
+        ax.scatter(xn_x[i], xn_y[i], xn_z[i], color='red')
+    plt.savefig('Python/Aufgabe2c2.pdf')
+    plt.close()
 
-#Aufgabe 2e
-xn = ROOT.TRandom3()
-xn = [xn.Rndm() for i in range(10000)]
+    #Aufgabe 2d
+    for i in range(10000): #Periodizität
+        if xn[i] == xn[0]:
+            print(i)
 
-xn_x = xn[0:]
-xn_y = np.append(xn[1:], xn[0])
-xn_z = np.append(xn[2:], (xn[0], xn[1]))
+    #Aufgabe 2e
+    xn = ROOT.TRandom3()
+    xn = [xn.Rndm() for i in range(10000)]
 
-plt.hist(xn, bins=10)
-plt.xlabel('Zufallszahl')
-plt.ylabel('Anzahl')
-plt.savefig('Python/Aufgabe2e1.pdf')
-plt.close()
+    xn_x = xn[0:]
+    xn_y = np.append(xn[1:], xn[0])
+    xn_z = np.append(xn[2:], (xn[0], xn[1]))
 
-plt.hist2d(xn_x, xn_y, bins=1000)
-plt.xlabel('$(x_i, x_{i+1})$')
-plt.ylabel('$(x_{i+1}, x_{i+2})$')
-plt.colorbar()
-plt.savefig('Python/Aufgabe2e2.pdf')
-plt.close()
+    plt.hist(xn, bins=10)
+    plt.xlabel('Zufallszahl')
+    plt.ylabel('Anzahl')
+    plt.savefig('Python/Aufgabe2e1.pdf')
+    plt.close()
 
-plt.figure()
-ax = plt.gca(projection='3d')
-for i in range(10000):
-    ax.scatter(xn_x[i], xn_y[i], xn_z[i], color='red')
-plt.savefig('Python/Aufgabe2e3.pdf')
-plt.close()
+    plt.hist2d(xn_x, xn_y, bins=1000)
+    plt.xlabel('$(x_i, x_{i+1})$')
+    plt.ylabel('$(x_{i+1}, x_{i+2})$')
+    plt.colorbar()
+    plt.savefig('Python/Aufgabe2e2.pdf')
+    plt.close()
+
+    plt.figure()
+    ax = plt.gca(projection='3d')
+    for i in range(10000):
+        ax.scatter(xn_x[i], xn_y[i], xn_z[i], color='red')
+    plt.savefig('Python/Aufgabe2e3.pdf')
+    plt.close()
+
+if __name__ == '__main__':
+	aufg()
