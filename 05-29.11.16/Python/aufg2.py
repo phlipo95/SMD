5d56a9b90dc4e8a3a85156ddb2880192cee0efe5
MaxSac <maximilian.sackel@tu-dortmund.de>
2016-11-27 16:52:12 +0100

    Alles was wir heute gemacht haben ...

--- 05-29.11.16/Python/aufg2.py
+++ 05-29.11.16/Python/aufg2.py
@@ -64,32 +64,89 @@ def g3(x):
     return -1.25*x
 
 x = np.linspace(min(x_P00),max(x_P01),100)
-plt.scatter(x_P01,y_P01, color='r', label='P_1')
-plt.scatter(x_P00,y_P00, color='b', label='P_0_10000')
-plt.plot(x,g1(x))
-plt.plot(x,g2(x))
-plt.plot(x,g3(x))
-plt.legend(loc='best')
 
 vec1 = np.matrix([x_P01,y_P01])
 vec2 = np.matrix([x_P00,y_P00])
 
 G1 = np.matrix([[1],[0]])
-G2 = np.matrix([[-3],[4]])
+G2 = np.matrix([[3],[-4]])
 G2 = G2/np.linalg.norm(G2)
-G3 = np.matrix([[-5],[4]])
+G3 = np.matrix([[5],[-4]])
 G3 = G3/np.linalg.norm(G3)
-#plt.plot(vec1[0,:],g1(G1[0]*vec1[0,:]), 'rx')
-#plt.plot(vec2[0,:],g1(G1[0]*vec2[0,:]), 'bx')
-#plt.plot(vec1[0,:],g2(G2[0]*vec1[0,:]), 'rx')
-#plt.plot(vec2[0,:],g2(G2[0]*vec2[0,:]), 'bx')
-plt.plot(vec1[0,:],g3(G3[0]*vec1[0,:]), 'rx')
-plt.plot(vec2[0,:],g3(G3[0]*vec2[0,:]), 'bx')
 
-plt.show()
+def z(vec, G):
+    return np.transpose(np.array(np.transpose(vec[:,:])*G))
+'''
+
+#Scatterplot
+plt.figure(1)
+plt.subplot(3,1,1)
+plt.scatter(x_P01,y_P01,color='r',alpha=0.1)
+plt.scatter(x_P00,y_P00,color='b',alpha=0.1)
+plt.plot(G1[0]*z(vec1,G1)[:],G1[1]*z(vec1,G1)[:], 'go',alpha=0.2)
+plt.plot(G1[0]*z(vec2,G1)[:],G1[1]*z(vec2,G1)[:], 'yo',alpha=0.2)
+plt.subplot(3,1,2)
+plt.scatter(x_P01,y_P01,color='r',alpha=0.2)
+plt.scatter(x_P00,y_P00,color='b',alpha=0.2)
+plt.plot(G2[0]*z(vec1,G2)[:],G2[1]*z(vec1,G2)[:], 'go',alpha=0.2)
+plt.plot(G2[0]*z(vec2,G2)[:],G2[1]*z(vec2,G2)[:], 'yo',alpha=0.2)
+plt.subplot(3,1,3)
+plt.scatter(x_P01,y_P01,color='r',alpha=0.2)
+plt.scatter(x_P00,y_P00,color='b',alpha=0.2)
+plt.plot(G3[0]*z(vec1,G3)[:],G3[1]*z(vec1,G3)[:], 'go',alpha=0.2)
+plt.plot(G3[0]*z(vec2,G3)[:],G3[1]*z(vec2,G3)[:], 'yo',alpha=0.2)
+plt.savefig('Scatter.pdf')
+plt.close()
+'''
+
+#Histogramme zum scatterplot
+plt.figure(1)
+plt.subplot(3,1,1)
+H11 = np.transpose(vec1[:,:])*G1
+plt.hist(H11,bins=20,alpha=0.2,label='P1',normed=1)
+H12 = np.transpose(vec2[:,:])*G1
+plt.hist(H12,bins=20,alpha=0.2,label='P0',normed=1)
+plt.legend(loc='best')
+
+plt.subplot(3,1,2)
+H21 = np.transpose(vec1[:,:])*G2
+plt.hist(H21,bins=20,alpha=0.2,label='P1',normed=1)
+H22= np.transpose(vec2[:,:])*G2
+plt.hist(H22,bins=20,alpha=0.2,label='P0',normed=1)
+plt.legend(loc='best')
+
+plt.subplot(3,1,3)
+H31 =  np.transpose(vec1[:,:])*G3
+plt.hist(H31,bins=20,alpha=0.2,label='P0',normed=1)
+H32 = np.transpose(vec2[:,:])*G3
+plt.hist(H32,bins=20,alpha=0.2,label='P1',normed=1)
+plt.legend(loc='best')
+plt.savefig('hist.pdf')
+plt.close()
+
+#Reinheit
+def rein(Ha1, Ha2):
+    x = np.linspace(min(Ha2).item(),max(Ha1).item(),10)
+    leH1 = np.array([])
+    leH2 = np.array([])
+    for a in x:
+        mask = Ha1 <= a
+        leH1 = np.append(leH1, len(np.squeeze(np.asarray(Ha1[mask]))))
+    for i in x:
+        masg = Ha2 <= i
+        print('Ha2',Ha2[masg])
+        print(len(Ha2[masg]))
+
+print('hier kommt H11',len(H11),type(H11))
+print('hier kommt H12',len(H12),type(H12))
+rein(H11,H12)
+
+
+
+
+
 '''
 Linerare Fischer Diskreminanzanalyse
-'''
 #Mittelwerte der x-/y-Messwerte
 my_1 = np.matrix([[np.mean(x_P01)],[np.mean(y_P01)]])
 my_2 = np.matrix([[np.mean(x_P00)],[np.mean(y_P00)]])
@@ -105,7 +162,7 @@ SW = S1 + S2
 SW1 = np.linalg.inv(SW)
 #SW1 = np.matrix([[SW[1,1],-SW[0,1]],[-SW[1,0],SW[0,0]]])
 #SW1 = SW1/np.linalg.det(SW)
-sB = (my_1 - my_2)*np.transpose(my_1 - my_2)
+#sB = (my_1 - my_2)*np.transpose(my_1 - my_2)
 #Eigenwerte der LDA
 lam = SW1*(my_1-my_2)
 print(lam)
@@ -113,3 +170,7 @@ print(lam)
 lamN = lam/np.linalg.norm(lam)
 print(lamN)
 
+plt.hist(np.transpose(vec1[:,:])*lamN,bins=20,alpha=0.2,label='0',normed=1)
+plt.hist(np.transpose(vec2[:,:])*lamN,bins=20,alpha=0.2,label='0',normed=1)
+plt.show()
+'''
