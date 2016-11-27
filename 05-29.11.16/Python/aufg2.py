28571b16cf0294d4663afd6a96b4267e9e977ab3
MaxSac <maximilian.sackel@tu-dortmund.de>
2016-11-27 11:07:05 +0100

    Aufgabe 2b

--- 05-29.11.16/Python/aufg2.py
+++ 05-29.11.16/Python/aufg2.py
@@ -64,22 +64,39 @@ def g3(x):
     return -1.25*x
 
 x = np.linspace(min(x_P00),max(x_P01),100)
-plt.scatter(x_P01,y_P01, color='r')
-plt.scatter(x_P00,y_P00, color='b')
+plt.scatter(x_P01,y_P01, color='r', label='P_1')
+plt.scatter(x_P00,y_P00, color='b', label='P_0_10000')
 plt.plot(x,g1(x))
 plt.plot(x,g2(x))
 plt.plot(x,g3(x))
-
-#Mittelwerte der x-/y-Messwerte
-my_1 = np.matrix([[np.mean(x_P01)],[np.mean(y_P01)]])
-my_2 = np.matrix([[np.mean(x_P00)],[np.mean(y_P00)]])
+plt.legend(loc='best')
 
 vec1 = np.matrix([x_P01,y_P01])
 vec2 = np.matrix([x_P00,y_P00])
 
+G1 = np.matrix([[1],[0]])
+G2 = np.matrix([[-3],[4]])
+G2 = G2/np.linalg.norm(G2)
+G3 = np.matrix([[-5],[4]])
+G3 = G3/np.linalg.norm(G3)
+#plt.plot(vec1[0,:],g1(G1[0]*vec1[0,:]), 'rx')
+#plt.plot(vec2[0,:],g1(G1[0]*vec2[0,:]), 'bx')
+#plt.plot(vec1[0,:],g2(G2[0]*vec1[0,:]), 'rx')
+#plt.plot(vec2[0,:],g2(G2[0]*vec2[0,:]), 'bx')
+plt.plot(vec1[0,:],g3(G3[0]*vec1[0,:]), 'rx')
+plt.plot(vec2[0,:],g3(G3[0]*vec2[0,:]), 'bx')
+
+plt.show()
+'''
+Linerare Fischer Diskreminanzanalyse
+'''
+#Mittelwerte der x-/y-Messwerte
+my_1 = np.matrix([[np.mean(x_P01)],[np.mean(y_P01)]])
+my_2 = np.matrix([[np.mean(x_P00)],[np.mean(y_P00)]])
+
 S1 = np.array([[0,0],[0,0]])
 S2 = np.array([[0,0],[0,0]])
-for i in range(5):
+for i in range(len(x_P01)):
     S1 = S1 + ((vec1[:,i] - my_1)*np.transpose(vec1[:,i] - my_1) )
     S2 = S2 + ((vec2[:,i] - my_1)*np.transpose(vec2[:,i] - my_1) )
 print('S1 = ', S1)
@@ -89,4 +106,10 @@ SW1 = np.linalg.inv(SW)
 #SW1 = np.matrix([[SW[1,1],-SW[0,1]],[-SW[1,0],SW[0,0]]])
 #SW1 = SW1/np.linalg.det(SW)
 sB = (my_1 - my_2)*np.transpose(my_1 - my_2)
-print('sB = ', sB)
+#Eigenwerte der LDA
+lam = SW1*(my_1-my_2)
+print(lam)
+#Normierter LDA Vektor
+lamN = lam/np.linalg.norm(lam)
+print(lamN)
+
