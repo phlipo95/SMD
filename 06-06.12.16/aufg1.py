7f0e0e885ec3a3ad2a2c16ac5e45c714d5c019f6
MaxSac <maximilian.sackel@tu-dortmund.de>
2016-12-05 12:24:57 +0100

    aufg 1 und 3

--- 06-06.12.16/aufg1.py
+++ 06-06.12.16/aufg1.py
@@ -26,10 +26,46 @@ def aufg1():
         return lamda
 
     #Stelle die Population in eine 1D Histogramm da
-    def plotHist(name, menge1, menge2, lam):
-        plt.hist(np.transpose(menge1[:,:])*lam,normed=True,alpha=0.2)
-        plt.hist(np.transpose(menge2[:,:])*lam,normed=True,alpha=0.2)
+    def plotHist(name, menge1, menge2, lam, lm1, lm2):
+        plt.hist(np.transpose(menge1[:,:])*lam,normed=True,alpha=0.2,label=lm1)
+        plt.hist(np.transpose(menge2[:,:])*lam,normed=True,alpha=0.2,label=lm2)
+        plt.legend(loc='best')
         plt.savefig(name)
+        plt.close()
+
+    #Berchne Effizienz und Reinheit
+    def berechneEffiRein(lam, menge1, menge2, plotname):
+        proj1 = np.transpose(menge1[:,:])*lam
+        proj2 = np.transpose(menge2[:,:])*lam
+        kE = np.asscalar(min([min(proj1),min(proj2)]))
+        gE = np.asscalar(max([max(proj1),max(proj2)]))
+        x = np.linspace(kE,gE,10)
+        pM1 = np.array([]); nM1 = np.array([]); pM2 = np.array([])
+        for a in x:
+            mask = menge1 >= a
+            masg = menge2 >= a
+            pM1 = np.append(pM1, len(menge1[mask]))
+            nM1 = np.append(nM1, len(menge1[~mask]))
+            pM2 = np.append(pM2, len(menge2[masg]))
+        reinheit = np.array(pM2/(pM2+pM1))
+        effizienz = np.array(pM2/(pM2+nM1))
+        plt.plot(x, reinheit,label='Reinheit')
+        plt.plot(x, effizienz,label='Effizienz')
+        plt.legend(loc='best')
+        plt.savefig(plotname)
+        plt.close()
+        return x, pM1, pM2
+
+    def SigZuUnt(x, sig, unt, name):
+        plt.figure(1)
+        plt.subplot(2,1,1)
+        plt.plot(x,sig/unt,label=r'Signal/Untergrund')
+        plt.legend(loc='best')
+        plt.subplot(2,1,2)
+        plt.plot(x,sig/np.sqrt(sig + unt),label=r'Signal/Untergrund')
+        plt.savefig(name.__add__('.pdf'))
+        plt.close()
+
 
     P1 = rooteinlesen("./zwei_populationen.root","P_1")
     P0 = rooteinlesen("./zwei_populationen.root","P_0_10000")
@@ -43,10 +79,12 @@ def aufg1():
     S_P1 = berechneKovS(P1, myP1)
 
     lam1 = berechneLDA(S_P0,S_P1,myP0,myP1)
+
+    plotHist('firstHist.pdf', P0, P1,lam1,'P0','P1')
+
+    x1, sig1, sig2 = berechneEffiRein(lam1,P0,P1,'blabla.pdf')
+    SigZuUnt(x1, sig1, sig2, 'abc')
     
-    plotHist('firstHist.pdf', P0, P1,lam1)
-
-
     berechneKovS(P1, myP1)
 if __name__ == '__main__':
     aufg1()
